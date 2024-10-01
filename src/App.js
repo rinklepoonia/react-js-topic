88f9da6f0d738c238f6ca3991182d066908a01a2
rinklepoonia <rinklepoonia889@gmail.com>
2024-10-01 10:09:36 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import LoopPractice from './components/LoopPractice';
-import RangeTable from './components/RangeTable';
 import RangeNestedLoop from './components/RangeNestedLoop';
 
 function App() {
   return (
     <>
       <RangeNestedLoop/>
-      {/* <LoopPractice /> */}
-      {/* <RangeTable/> */}
     </>
   );
 }
